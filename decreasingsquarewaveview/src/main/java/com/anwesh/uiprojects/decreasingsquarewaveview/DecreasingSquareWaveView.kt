f939dc19af1090462773e4408b2a4f596b7f4094
Anwesh43 <anweshthecool0@gmail.com>
2018-07-12 10:33:48 +0530

    adding decreasingsquarewaveview

--- decreasingsquarewaveview/src/main/java/com/anwesh/uiprojects/decreasingsquarewaveview/DecreasingSquareWaveView.kt
+++ decreasingsquarewaveview/src/main/java/com/anwesh/uiprojects/decreasingsquarewaveview/DecreasingSquareWaveView.kt
@@ -3,3 +3,28 @@ package com.anwesh.uiprojects.decreasingsquarewaveview
 /**
  * Created by anweshmishra on 12/07/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.content.Context
+
+val nodes : Int = 5
+class DecreasingSquareWaveView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
